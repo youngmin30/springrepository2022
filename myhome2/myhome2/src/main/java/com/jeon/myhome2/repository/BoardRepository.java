b52e80afbb44466c5eee926dd456448589c4d46c
youngmin30 <jjymjj@naver.com>
2022-02-05 20:31:54 +0900

    BoardApiController에서 title로 검색해서 json 자료 보기 완료

--- myhome2/myhome2/src/main/java/com/jeon/myhome2/repository/BoardRepository.java
+++ myhome2/myhome2/src/main/java/com/jeon/myhome2/repository/BoardRepository.java
@@ -3,8 +3,10 @@ package com.jeon.myhome2.repository;
 import com.jeon.myhome2.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-
+    List<Board> findByTitle(String title);
 
 }
